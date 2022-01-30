ffc57698d8796d7b3b0db3d9071b16eaebe083b6
C07733 <37982387+C07733@users.noreply.github.com>
2022-01-30 01:09:06 -0300

    Updated config files

---
+++ .config/sxhkd/scripts/hidePolybar.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+polybarHide=/tmp/.polybarHide
+
+if [ ! -f "$polybarHide" ]; then
+    touch $polybarHide
+    bspc config top_padding 0  
+    hideIt.sh -N '^polybar-example_HDMI2$' -S -d top -p 0 & disown 
+else 
+  TOGGLE=/tmp/.toggle
+  if [ ! -f "$TOGGLE" ]; then
+      touch $TOGGLE
+      bspc config top_padding 0 && hideIt.sh -N '^polybar-example_HDMI2$' -t 
+  else
+      rm $TOGGLE
+      bspc config top_padding 28 && hideIt.sh -N '^polybar-example_HDMI2$' -t
+  fi
+fi
