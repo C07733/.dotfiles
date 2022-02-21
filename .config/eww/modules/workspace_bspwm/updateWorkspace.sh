2773a4144de4e24f0a4796255a1502ca46a737bb
C07733 <37982387+C07733@users.noreply.github.com>
2022-02-21 16:59:57 -0300

    Updated config files

--- .config/eww/modules/workspace_bspwm/updateWorkspace.sh
+++ .config/eww/modules/workspace_bspwm/updateWorkspace.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+if ! (set -o noclobber ; echo > /tmp/global.lock) ; then
+    exit 1  # the global.lock already exists
+fi
+
 DIR="$(dirname "$(realpath "$0")")"
 #echo $DIR
 
 $DIR/clearWorkspace.sh
 $DIR/desktopIconizer.sh
-
 
 bspc subscribe node_swap node_transfer node_add node_remove | while read -r _ ; do
     $DIR/clearWorkspace.sh
